1165387b987c3c8cbecb3a1197b18490667d80fb
gepukz <michaelwinaryong@gmail.com>
2015-09-26 11:03:29 +0700

    toolbar masih error

---
+++ Tes/app/src/main/java/com/example/user/tes/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.user.tes;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.support.v4.app.FragmentTransaction;
+
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity implements viewdata_fragment.fab_onclick {
+   public static ArrayList<String> list_nama;
+   public static ArrayList<Integer> list_umur;
+    //Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+      list_nama = new ArrayList<String>();
+      list_umur = new ArrayList<Integer>();
+
+        list_nama.add("ronaldo");
+        list_nama.add("messi");
+
+        list_umur.add(30);
+        list_umur.add(28);
+
+
+        
+        // Create an instance of ExampleFragment
+        viewdata_fragment firstFragment = new viewdata_fragment();
+
+        // In case this activity was started with special instructions from an Intent,
+        // pass the Intent's extras to the fragment as arguments
+        //firstFragment.setArguments(getIntent().getExtras());
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, firstFragment).commit();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onbuttonclick() {
+        insertdata_fragment newFragment = new insertdata_fragment();
+        //Bundle args = new Bundle();
+        //args.putInt(ArticleFragment.ARG_POSITION, position);
+        //newFragment.setArguments(args);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+}
